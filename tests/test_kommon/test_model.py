aee9adb7e78d87b3c0d5cbcbb7dbafe12de2ca54
AntaresT <luizeduardovbe@gmail.com>
2019-08-26 20:51:11 -0300

    Add Fixture to Base Model Test

--- tests/test_kommon/test_model.py
+++ tests/test_kommon/test_model.py
@@ -1,15 +1,22 @@
+import pytest
+
 from datetime import datetime
 
 from kommon.db import session
 from kommon.models import BaseModel
 
 
-def test_base_model():
+@pytest.fixture(name="base_model")
+def fixture_base_model():
     model = BaseModel()
     session.add(model)
     session.commit()
 
-    assert type(model.id) == int
-    assert type(model.created_at) == datetime
-    assert model.updated_at is None
-    assert model.__tablename__ == "basemodel"
+    return model
+
+
+def test_base_model(base_model):
+    assert type(base_model.id) == int
+    assert type(base_model.created_at) == datetime
+    assert base_model.updated_at is None
+    assert base_model.__tablename__ == "basemodel"
